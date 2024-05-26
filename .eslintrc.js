89ad007e0d990621cce5ac3ca4d3ab383e0f427c
Yamami <yamami.fujita@gmail.com>
2024-05-27 01:10:26 +0700

    add eslint and prettier config files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      { endOfLine: "auto" }
+    ],
+  },
+};
